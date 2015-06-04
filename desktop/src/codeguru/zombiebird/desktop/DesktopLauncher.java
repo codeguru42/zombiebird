869c50c881095e5bf78e0fe8c97419c337a3b13b
Code Guru <codeguru42@gmail.com>
2015-06-04 09:34:12 -0600

    Initial commit

---
+++ desktop/src/codeguru/zombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package codeguru.zombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import codeguru.zombiebird.ZombieBirdGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieBirdGame(), config);
+	}
+}
---
+++ ios/src/codeguru/zombiebird/IOSLauncher.java
@@ -0,0 +1,22 @@
+package codeguru.zombiebird;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import codeguru.zombiebird.ZombieBirdGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new ZombieBirdGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

9591f37075e0947b22bb35c0ceff6c098d32353c
Code Guru <codeguru42@gmail.com>
2015-06-04 10:16:01 -0600

    Add `GameScreen`

--- core/src/codeguru/zombiebird/ZombieBirdGame.java
+++ core/src/codeguru/zombiebird/ZombieBirdGame.java
@@ -1,11 +1,13 @@
 package codeguru.zombiebird;
 
+import codeguru.zombiebird.screens.GameScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 
 public class ZombieBirdGame extends Game {
-	@Override
-	public void create () {
-		Gdx.app.log("ZBGame", "created");
-	}
+    @Override
+    public void create() {
+        Gdx.app.log("ZBGame", "created");
+        setScreen(new GameScreen());
+    }
 }

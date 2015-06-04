85fbb2188e2f9f7ebbffbe6116ec095ba9671b17
Code Guru <codeguru42@gmail.com>
2015-06-04 10:37:06 -0600

    Add title and change size of desktop window

--- desktop/src/codeguru/zombiebird/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/zombiebird/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import codeguru.zombiebird.ZombieBirdGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new ZombieBirdGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Zombie Bird";
+        config.width = 272;
+        config.height = 408;
+        new LwjglApplication(new ZombieBirdGame(), config);
+    }
 }

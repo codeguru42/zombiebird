8a094b7a2465cf876aa5c5be1470c777a9e5dba2
Code Guru <codeguru42@gmail.com>
2015-06-04 10:39:29 -0600

    Add an `OrthographicCamera` to `GameRenderer`

--- core/src/codeguru/zombiebird/gameworld/GameRenderer.java
+++ core/src/codeguru/zombiebird/gameworld/GameRenderer.java
@@ -1,11 +1,16 @@
 package codeguru.zombiebird.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameRenderer {
+    private final OrthographicCamera cam;
     private final GameWorld myWorld;
 
     public GameRenderer(GameWorld world) {
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, 204);
+
         myWorld = world;
     }
 

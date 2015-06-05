7f7159451475165ea695ba3a164223026704565e
Code Guru <codeguru42@gmail.com>
2015-06-04 19:30:07 -0600

    Render grass and pipe images

--- core/src/codeguru/zombiebird/gameworld/GameRenderer.java
+++ core/src/codeguru/zombiebird/gameworld/GameRenderer.java
@@ -1,6 +1,9 @@
 package codeguru.zombiebird.gameworld;
 
 import codeguru.zombiebird.gameobjects.Bird;
+import codeguru.zombiebird.gameobjects.Grass;
+import codeguru.zombiebird.gameobjects.Pipe;
+import codeguru.zombiebird.gameobjects.ScrollHandler;
 import codeguru.zombiebird.helpers.AssetLoader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -17,6 +20,9 @@ public class GameRenderer {
     private int midPointY;
     private int gameHeight;
     private Bird bird;
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
 
     public GameRenderer(GameWorld world, int gameHeight, int midPointY) {
         myWorld = world;
@@ -36,6 +42,12 @@ public class GameRenderer {
 
     private void initGameObjects() {
         bird = myWorld.getBird();
+        scroller = myWorld.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
     }
 
     public void render(float runTime) {
@@ -48,31 +60,21 @@ public class GameRenderer {
         shapeRenderer.setColor(55 / 255.0f, 80 / 255.0f, 100 / 255.0f, 1);
         shapeRenderer.rect(0, 0, 136, midPointY + 66);
 
-        // Draw Grass
-        shapeRenderer.setColor(111 / 255.0f, 186 / 255.0f, 45 / 255.0f, 1);
-        shapeRenderer.rect(0, midPointY + 66, 136, 11);
-
         // Draw Dirt
         shapeRenderer.setColor(147 / 255.0f, 80 / 255.0f, 27 / 255.0f, 1);
         shapeRenderer.rect(0, midPointY + 77, 136, 52);
 
-        // End ShapeRenderer
         shapeRenderer.end();
 
-        // Begin SpriteBatch
         batcher.begin();
-        // Disable transparency
-        // This is good for performance when drawing images that do not require
-        // transparency.
         batcher.disableBlending();
         batcher.draw(AssetLoader.bg, 0, midPointY + 23, 136, 43);
 
-        // The bird needs transparency, so we enable that again.
+        drawGrass();
+        drawPipes();
         batcher.enableBlending();
+        drawSkulls();
 
-        // Draw bird at its coordinates. Retrieve the Animation object from
-        // AssetLoader
-        // Pass in the runTime variable to get the current frame.
         if (bird.shouldntFlap()) {
             batcher.draw(AssetLoader.bird, bird.getX(), bird.getY(),
                     bird.getWidth() / 2.0f, bird.getHeight() / 2.0f,
@@ -87,5 +89,46 @@ public class GameRenderer {
 
         // End SpriteBatch
         batcher.end();
+    }
+    private void drawGrass() {
+        // Draw the grass
+        batcher.draw(AssetLoader.grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(AssetLoader.grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        batcher.draw(AssetLoader.skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(AssetLoader.skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(AssetLoader.skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(AssetLoader.skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        batcher.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
     }
 }
--- core/src/codeguru/zombiebird/gameworld/GameWorld.java
+++ core/src/codeguru/zombiebird/gameworld/GameWorld.java
@@ -1,20 +1,28 @@
 package codeguru.zombiebird.gameworld;
 
 import codeguru.zombiebird.gameobjects.Bird;
+import codeguru.zombiebird.gameobjects.ScrollHandler;
 
 public class GameWorld {
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY) {
         bird = new Bird(33, midPointY - 5, 17, 12);
+        scroller = new ScrollHandler(midPointY + 66);
     }
 
     public void update(float delta) {
         bird.update(delta);
+        scroller.update(delta);
     }
 
     public Bird getBird() {
         return bird;
 
+    }
+
+    public ScrollHandler getScroller() {
+        return scroller;
     }
 }

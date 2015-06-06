23949cc6e28832db06695858ce239495595e1137
Code Guru <codeguru42@gmail.com>
2015-06-06 09:30:25 -0600

    Transition between game states

--- core/src/codeguru/zombiebird/helpers/InputHandler.java
+++ core/src/codeguru/zombiebird/helpers/InputHandler.java
@@ -1,18 +1,30 @@
 package codeguru.zombiebird.helpers;
 
 import codeguru.zombiebird.gameobjects.Bird;
+import codeguru.zombiebird.gameworld.GameWorld;
 import com.badlogic.gdx.InputAdapter;
 
 public class InputHandler extends InputAdapter {
+    private GameWorld myWorld;
     private Bird myBird;
 
-    public InputHandler(Bird bird) {
-        myBird = bird;
+    public InputHandler(GameWorld myWorld) {
+        this.myWorld = myWorld;
+        myBird = myWorld.getBird();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (myWorld.isReady()) {
+            myWorld.start();
+        }
+
         myBird.onClick();
+
+        if (myWorld.isGameOver()) {
+            myWorld.restart();
+        }
+
         return true;
     }
 }

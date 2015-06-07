77586ffbd4c63919d80bba5487685bd5ff2aa8b3
Code Guru <codeguru42@gmail.com>
2015-06-07 12:37:32 -0600

    Add `Value` and `ValueAccessor` in order to be able to tween a `float`

---
+++ core/src/codeguru/zombiebird/tween/Value.java
@@ -0,0 +1,13 @@
+package codeguru.zombiebird.tween;
+
+public class Value {
+    private float val = 1.0f;
+
+    public float getValue() {
+        return val;
+    }
+
+    public void setValue(float newVal) {
+        val = newVal;
+    }
+}
